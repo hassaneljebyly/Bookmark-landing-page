21a3ed1ffb26ae1d41c1a563c51c2e2da17b1f43
hassaneljebyly <hassaneljeby@gmail.com>
2023-06-12 21:49:05 +0100

    modified header

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,23 @@
+const buttons = Array.from(document.getElementsByClassName('button'));
+
 const toggleMenuBtn = document.getElementById('toggle-menu-btn');
 const mainNav = document.getElementsByClassName('main-nav')[0];
 
 toggleMenuBtn.addEventListener('click', () => {
   mainNav.classList.toggle('open');
+});
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = Math.floor(e.clientX - rect.left);
+    const y = Math.floor(e.clientY - rect.top);
+    const targetedButton = window.getComputedStyle(e.target);
+    const clickAnimationDuration = Number(targetedButton.getPropertyValue('--click-animation-duration').replace('ms', ''));
+    e.target.classList.add('button--clicked');
+    button.style = `--top:${y}px;--left:${x}px;`;
+    setTimeout(() => {
+      e.target.classList.remove('button--clicked');
+    }, clickAnimationDuration);
+  });
 });
