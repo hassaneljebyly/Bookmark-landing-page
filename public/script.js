217001b285142764a452f12f0c8f7d6b99781f07
hassaneljebyly <hassaneljeby@gmail.com>
2024-01-23 20:51:02 +0100

    finished main section

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+const PRIMARY_HEADER = document.getElementById('primary_header');
+const MENU_BTN = document.getElementById('menu_btn');
+const PRIMARY_NAV = document.getElementById('primary_nav');
+const NAV_LIST = document.getElementById('nav_list');
+const NAV_SOCIAL = document.getElementById('nav_social');
+
 // stop annoying animation during resize
 (function () {
   const classes = document.body.classList;
@@ -15,11 +21,6 @@
   });
 })();
 
-const MENU_BTN = document.getElementById('menu_btn');
-const PRIMARY_NAV = document.getElementById('primary_nav');
-const NAV_LIST = document.getElementById('nav_list');
-const NAV_SOCIAL = document.getElementById('nav_social');
-
 MENU_BTN.addEventListener('click', () => {
   PRIMARY_NAV.classList.toggle('nav--open');
   let open = !PRIMARY_NAV.classList.contains('nav--open');
@@ -27,3 +28,22 @@ MENU_BTN.addEventListener('click', () => {
   NAV_LIST.setAttribute('aria-hidden', open);
   NAV_SOCIAL.setAttribute('aria-hidden', open);
 });
+
+// adding shadow on header when scrolling
+const scrollObserverDiv = document.createElement('div');
+scrollObserverDiv.setAttribute('data-scroll-observer', '');
+// insert scroll observer div after primary header
+document.body.insertBefore(scrollObserverDiv, PRIMARY_HEADER);
+//
+const options = {
+  root: null,
+  rootMargin: '100px 0px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver((entries, observer) => {
+  let isIntersecting = entries[0].isIntersecting;
+  PRIMARY_HEADER.classList.toggle('scroll-is-triggered', !isIntersecting);
+}, options);
+
+observer.observe(scrollObserverDiv);
